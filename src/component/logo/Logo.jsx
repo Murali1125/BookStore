68ee59fa36edf09219b9e7cdef9e42726e4629f7
Saksham27 <geekysaksham27@gmail.com>
2020-07-10 11:06:35 +0530

    Added : created Logo Component

---
+++ src/component/logo/Logo.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import bookLogo from "./../../assets/logo.svg"
+
+export default class Logo extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
