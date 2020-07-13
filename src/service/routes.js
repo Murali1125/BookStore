32dfaec2db75bec582684d98d15dfff3ff35e55b
ruchika <ruchika6698@gmail.com>
2020-07-13 19:38:37 +0530

    Added:Routes for login

--- src/service/routes.js
+++ src/service/routes.js
@@ -3,6 +3,7 @@ import { Switch, Route } from "react-router-dom";
 import  Register from "./../pages/Register/Register";
 import  Login from "./../pages/Login/Login";
 import  Dashboard from "./../pages/Dashboard/DashboardAdmin";
+import Checkout from "./../pages/checkout/Checkout";
 import Header from "../component/header/Header";
 
 function Routes() {
@@ -12,6 +13,7 @@ function Routes() {
       <Route path="/login" component={Login} />
       <Route path="/header" component={Header}/>
       <Route path="/dashboard" component={Dashboard}/>
+      <Route path="/checkout" component={Checkout}/>
     </Switch>
   );
 }
