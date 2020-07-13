20a73f8f5ed152fb97ab506d7101d39d8a303db7
MuraliKrishnaReddy <60267181+Murali1125@users.noreply.github.com>
2020-07-14 01:44:58 +0530

    books table fields Enabled for editing

--- src/pages/Dashboard/BookDetailsTable.jsx
+++ src/pages/Dashboard/BookDetailsTable.jsx
@@ -1,77 +1,268 @@
-import React from 'react';
-import { withStyles, makeStyles } from '@material-ui/core/styles';
+import React, { useState }  from 'react';
+import {TextField} from '@material-ui/core';
+import PropTypes from 'prop-types';
+import { makeStyles, useTheme,withStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
+import TableFooter from '@material-ui/core/TableFooter';
+import TablePagination from '@material-ui/core/TablePagination';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
+import IconButton from '@material-ui/core/IconButton';
+import FirstPageIcon from '@material-ui/icons/FirstPage';
+import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
+import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
+import LastPageIcon from '@material-ui/icons/LastPage';
+import TableHead from '@material-ui/core/TableHead';
+import { useEffect } from 'react';
+import EditOutlinedIcon from '@material-ui/icons/EditOutlined';
+import DeleteOutlineOutlinedIcon from '@material-ui/icons/DeleteOutlineOutlined';
 
 const StyledTableCell = withStyles((theme) => ({
-  head: {
-    backgroundColor: theme.palette.common.black,
-    color: theme.palette.common.white,
-  },
-  body: {
-    fontSize: 14,
-  },
-}))(TableCell);
-
-const StyledTableRow = withStyles((theme) => ({
-  root: {
-    '&:nth-of-type(odd)': {
-      backgroundColor: theme.palette.action.hover,
+    head: {
+      backgroundColor: theme.palette.common.black,
+      color: theme.palette.common.white,
     },
-  },
+    body: {
+      fontSize: 14,
+    },
+  }))(TableCell);
+  
+const StyledTableRow = withStyles((theme) => ({
+root: {
+    '&:nth-of-type(odd)': {
+    backgroundColor: theme.palette.action.hover,
+    },
+},
 }))(TableRow);
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
+const useStyles = makeStyles({
+table: {
+    minWidth: 700,
+},
+});
+  
+const useStyles1 = makeStyles((theme) => ({
+  root: {
+    flexShrink: 0,
+    marginLeft: theme.spacing(2.5),
+  },
+}));
+
+function TablePaginationActions(props) {
+  const classes = useStyles1();
+  const theme = useTheme();
+  //const classes = useStyles();
+  const { count, page, rowsPerPage, onChangePage } = props;
+
+  const handleFirstPageButtonClick = (event) => {
+    onChangePage(event, 0);
+  };
+
+  const handleBackButtonClick = (event) => {
+    onChangePage(event, page - 1);
+  };
+
+  const handleNextButtonClick = (event) => {
+    onChangePage(event, page + 1);
+  };
+
+  const handleLastPageButtonClick = (event) => {
+    onChangePage(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
+  };
+
+  return (
+    <div className={classes.root}>
+      <IconButton
+        onClick={handleFirstPageButtonClick}
+        disabled={page === 0}
+        aria-label="first page"
+      >
+        {theme.direction === 'rtl' ? <LastPageIcon /> : <FirstPageIcon />}
+      </IconButton>
+      <IconButton onClick={handleBackButtonClick} disabled={page === 0} aria-label="previous page">
+        {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+      </IconButton>
+      <IconButton
+        onClick={handleNextButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="next page"
+      >
+        {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+      </IconButton>
+      <IconButton
+        onClick={handleLastPageButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="last page"
+      >
+        {theme.direction === 'rtl' ? <FirstPageIcon /> : <LastPageIcon />}
+      </IconButton>
+    </div>
+  );
 }
 
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
+TablePaginationActions.propTypes = {
+  count: PropTypes.number.isRequired,
+  onChangePage: PropTypes.func.isRequired,
+  page: PropTypes.number.isRequired,
+  rowsPerPage: PropTypes.number.isRequired,
+};
 
-const useStyles = makeStyles({
+const useStyles2 = makeStyles({
   table: {
-    minWidth: 700,
+    minWidth: 500,
   },
 });
 
-export default function CustomizedTables() {
-  const classes = useStyles();
+export default function CustomPaginationActionsTable() {
+  const classes = useStyles2();
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+  const [data,setData] = useState([]);
+  const [editable,setEdit] = useState(true)
+  const [indexOfEditableBook,setEditIndex] = useState();
+  const emptyRows = rowsPerPage - Math.min(rowsPerPage, data.length - page * rowsPerPage);
+  
 
+  useEffect(()=>{
+    let tempBooksArry=[];
+        for(var i=0;i<20;i++){            
+            tempBooksArry.push({
+                title : 'StructureAnalysis',
+                decription : 'Structural analysis is the determination of the effects of loads on physical structures and their components. Structures subject to this type of analysis include all that must withstand loads, such as buildings, bridges, aircraft and ships.',
+                author : 'S.S. Bhavikatti',
+                imageUrl : 'https://panchayatrajengineers.files.wordpress.com/2019/02/11820192337156313445039947930760.jpg?w=640',
+                price : '200',
+                quantity : '15',
+            })        
+        }
+        setData(tempBooksArry);
+  },[])
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  let indexOfSelectedEditBook ='';
+  const EditBook = (index)=>{
+    setEditIndex(index);
+    setEdit(!editable);
+  }
   return (
     <TableContainer component={Paper}>
-      <Table className={classes.table} aria-label="customized table">
-        <TableHead>
+      <Table className={classes.table} aria-label="custom pagination table">
+      <TableHead>
           <TableRow>
-            <StyledTableCell>Dessert (100g serving)</StyledTableCell>
-            <StyledTableCell align="right">Calories</StyledTableCell>
-            <StyledTableCell align="right">Fat&nbsp;(g)</StyledTableCell>
-            <StyledTableCell align="right">Carbs&nbsp;(g)</StyledTableCell>
-            <StyledTableCell align="right">Protein&nbsp;(g)</StyledTableCell>
+            <StyledTableCell align="center">Book</StyledTableCell>
+            <StyledTableCell align="center">Title</StyledTableCell>
+            <StyledTableCell align="center">Author</StyledTableCell>
+            <StyledTableCell align="center">Price</StyledTableCell>
+            <StyledTableCell align="center">Quantity</StyledTableCell>
+            <StyledTableCell align="center">Edit</StyledTableCell>
+            <StyledTableCell align="center">Remove</StyledTableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
-            <StyledTableRow key={row.name}>
-              <StyledTableCell component="th" scope="row">
-                {row.name}
-              </StyledTableCell>
-              <StyledTableCell align="right">{row.calories}</StyledTableCell>
-              <StyledTableCell align="right">{row.fat}</StyledTableCell>
-              <StyledTableCell align="right">{row.carbs}</StyledTableCell>
-              <StyledTableCell align="right">{row.protein}</StyledTableCell>
+          {(rowsPerPage > 0
+            ? data.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+            : data
+          ).map((book,index) => (
+            <StyledTableRow key={index}>
+                <StyledTableCell  align="center">
+                    <img src ={book.imageUrl} alt ={book.title} 
+                         style={{height : '50px', width:'75px'}}
+                    />
+                </StyledTableCell>
+                <StyledTableCell align="center"> 
+                    {  (editable)  && (index === indexOfEditableBook) ?  
+                        <TextField                                             
+                            value={book.title} 
+                            variant="outlined"
+                            size='small'
+                            style={{maxWidth : '70%'}} 
+                            InputProps={{
+                                disableUnderline: true,                                                
+                            }} 
+                        /> : book.title
+                    }
+                </StyledTableCell>
+                <StyledTableCell align="center">
+                    { (editable)  && (index === indexOfEditableBook) ?  
+                        <TextField                             
+                            value={book.author} 
+                            style={{width : '70%'}} 
+                            variant="outlined"
+                            size='small'
+                            InputProps={{
+                                disableUnderline: true,                                                
+                            }} 
+                        /> : book.author
+                    }
+                </StyledTableCell>
+                <StyledTableCell align="center">
+                    { (editable)  && (index === indexOfEditableBook) ?  
+                        <TextField 
+                            style={{width : '30%'}} 
+                            variant="outlined"
+                            size='small'
+                            value={book.price} 
+                            InputProps={{
+                                disableUnderline: true,                                                
+                            }} 
+                        /> : book.price
+                    }
+                </StyledTableCell>
+                <StyledTableCell align="center">
+                    { (editable)  && (index === indexOfEditableBook) ?  
+                        <TextField
+                            style={{width : '30%'}} 
+                            value={book.quantity} 
+                            variant="outlined"
+                            size='small'
+                            InputProps={{
+                                disableUnderline: true,                                                
+                            }} 
+                        /> : book.quantity
+                    }
+                </StyledTableCell>
+                <StyledTableCell align="center">
+                    <IconButton onClick={()=>EditBook(index)}>
+                        <EditOutlinedIcon/>
+                    </IconButton>
+                </StyledTableCell>
+                <StyledTableCell align="center">
+                    <IconButton>
+                        <DeleteOutlineOutlinedIcon/>
+                    </IconButton>
+                </StyledTableCell>
             </StyledTableRow>
           ))}
         </TableBody>
+        <TableFooter>
+          <TableRow>
+            <TablePagination
+              rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
+              colSpan={3}
+              count={data.length}
+              rowsPerPage={rowsPerPage}
+              page={page}
+              SelectProps={{
+                inputProps: { 'aria-label': 'rows per page' },
+                native: true,
+              }}
+              onChangePage={handleChangePage}
+              onChangeRowsPerPage={handleChangeRowsPerPage}
+              ActionsComponent={TablePaginationActions}
+            />
+          </TableRow>
+        </TableFooter>
       </Table>
     </TableContainer>
   );
