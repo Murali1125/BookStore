efd40fffd756434a7f550089e94c78bb92073c33
ruchika <ruchika6698@gmail.com>
2020-07-14 16:39:20 +0530

    Added:Axios Services for Post Method

---
+++ src/service/axiosServices.jsx
@@ -0,0 +1,15 @@
+import axios from 'axios'
+
+export default class AxiosService{
+    Post(url,data,isHeaderRequired){
+        return axios.post(url,data,isHeaderRequired);
+    }
+
+    Get(url,data,isHeaderRequired){
+        return axios.get(url,data,isHeaderRequired);
+    }
+    
+    Delete(url,id,isHeaderRequired){
+        return axios.delete(url,id,isHeaderRequired);
+    }
+}
