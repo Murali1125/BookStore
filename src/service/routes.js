d349828c0f651719564a206894c6e289caa2886b
Saksham27 <geekysaksham27@gmail.com>
2020-07-14 11:58:17 +0530

    Added : Book component

--- src/service/routes.js
+++ src/service/routes.js
@@ -5,6 +5,7 @@ import  Login from "./../pages/Login/Login";
 import  Dashboard from "./../pages/Dashboard/DashboardAdmin";
 import Header from "../component/header/Header";
 import Footer from "../component/Footer/Footer";
+import Book from "../component/Book/Book";
 
 function Routes() {
   return (
@@ -14,6 +15,7 @@ function Routes() {
       <Route path="/header" component={Header}/>
       <Route path="/dashboard" component={Dashboard}/>
       <Route path="/footer" component={Footer}/>
+      <Route path="/book" component={Book}/>
     </Switch>
   );
 }
