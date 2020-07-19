4521e7e608ca120f10506be593460512a019b70f
Saksham27 <geekysaksham27@gmail.com>
2020-07-19 20:22:47 +0530

    Added : test for Book component

--- src/_tests_/Book.test.js
+++ src/_tests_/Book.test.js
@@ -6,7 +6,13 @@ import "./../setupTests";
 describe("Book component", () => {
   // testing redering of header component
   it("should render without throwing any error", () => {
-    const wrapper = shallow(<Book />);
+    const book = {
+      title: "BookTitle",
+      description: "BookDescription",
+      price: 100,
+      author: "BookAuthor",
+    };
+    const wrapper = shallow(<Book>{book}</Book>);
     expect(wrapper.exists()).toBe(true);
   });
 });
