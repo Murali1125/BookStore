488555e444837736326a7708b9ffb89adf8623c3
ruchika <ruchika6698@gmail.com>
2020-07-22 19:06:00 +0530

    Added:Admin routing for dashboard

--- src/pages/Store/Store.jsx
+++ src/pages/Store/Store.jsx
@@ -104,7 +104,7 @@ export default class Store extends Component {
     localStorage.removeItem("Address");
     localStorage.removeItem("City");
     localStorage.removeItem("Phone Number");
-    this.props.history.push("/");
+    this.props.history.push("/login");
   };
 
   componentDidMount() {
